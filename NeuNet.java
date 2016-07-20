99d78c577ad46e9090553e26e2e38449e43b2d9e
wallymediacenter <mcassist@M1611671.local>
2016-07-20 16:34:46 -0400

    Test initialized

--- NeuNet.java
+++ NeuNet.java
@@ -4,73 +4,46 @@ public class NeuNet{
   /*Fields*/
 
   //Input layer
-  public double[][] s; //1x5
+  public double[][] input; //1x2
 
   //W1
-  public double[][] w1; //5x6
-  public double[][] deltaW1; //5x6
+  public double[][] w1; //2X3
+  public double[][] deltaW1; //2x3
 
   //Hidden layer 1
-  public double[][] z1; //1x6
-  public double[][] a1; //1x6
+  public double[][] z1; //1x3
+  public double[][] a1; //1x3
 
-  //W2
-  public double[][] w2; //6x5
-  public double[][] deltaW2; //6x5
-
-  //Hidden layer 2
-  public double[][] z2; //1x5
-  public double[][] a2; //1x5
-
-  //W3
-  public double[][] w3; //5x3
-  public double[][] deltaW3; //5x3
+  //w2
+  public double[][] w2; //3x1
+  public double[][] deltaW2; //3x1
 
   //Output layer
-  public double[][] z3; //1x3
-  public double[][] q; //1x3
-  public double[][] oldQ;
-
-
+  public double[][] z2; //1x1
+  public double[][] output; //1x1
 
   /*Constructor*/
   public NeuNet(){
     //Input layer
-    s = new double[1][5];
-
-    s[0][0] = 0.1;
-    s[0][1] = 0.2;
-    s[0][2] = 0.3;
-    s[0][3] = 0.4;
-    s[0][4] = 0.5;
+    input = new double[1][2];
 
     //W1
-    w1 = new double[5][6];
-    deltaW1 = new double[5][6];
+    w1 = new double[2][3];
+    deltaW1 = new double[2][3];
     random(w1);
 
     //Hidden layer 1
-    z1 = new double[1][6];
-    a1 = new double[1][6];
+    z1 = new double[1][3];
+    a1 = new double[1][3];
 
     //W2
-    w2 = new double[6][5];
-    deltaW2 = new double[6][5];
+    w2 = new double[3][1];
+    deltaW2 = new double[3][1];
     random(w2);
 
-    //Hidden layer 2
-    z2 = new double[1][5];
-    a2 = new double[1][5];
-
-    //W2
-    w3 = new double[5][3];
-    deltaW3 = new double[5][3];
-    random(w3);
-
     //Output layer
-    z3 = new double[1][3];
-    q = new double[1][3];
-    oldQ = new double[1][3];
+    z2 = new double[1][1];
+    output = new double[1][1];
 
   }
 
@@ -116,7 +89,7 @@ public class NeuNet{
     double[][] c = new double[a.length][a[0].length];
     for(int i = 0; i< a.length; i++){
       for(int j = 0; j < a[0].length; j++){
-        c[i][j] = a[i][j] - b[i][j];
+        c[i][j] = a[i][j] + b[i][j];
       }
     }
     return c;
@@ -155,26 +128,6 @@ public class NeuNet{
     return t;
   }
 
-  public double[][] max(double[][] q, double r, double gamma){
-    double[][] newQ = new double[q.length][q[0].length];
-
-    for(int i = 0; i < q.length; i++){
-      for(int j = 0; j < q[0].length; j++){
-        newQ[i][j] = q[i][j];
-      }
-    }
-    int max = 0;
-
-    if(q.length > 1){System.out.println("Error this function ony accept single row matrix");}
-    else{
-      if(q[0][0] > q[0][1]) max = 0; else max = 1;
-      if(q[0][max] < q[0][2]) max = 2;
-    }
-
-    newQ[0][max] = r + gamma.newQ[0][max];
-    return newQ;
-  }
-
   //*Learning aid*//
   //Sigmoid function
   public static double s(double x){
@@ -208,49 +161,32 @@ public class NeuNet{
   }
 
   /////////////////////////**///////////////////////////////
-  public void forward(double[][] sensorInput){
+  public void forward(double[][] newInput){
 
-    s = sensorInput;
+    input = newInput;
 
     //Hidden layer 1
-    z1 = mul(s, w1);
+    z1 = mul(input, w1);
     a1 = s(z1);
 
     //Hidden layer 2
     z2 = mul(a1, w2);
-    a2 = s(z2);
-
-    //Output
-    z3 = mul(a2, w3);
-    q = s(z3);
-
-    qTarget = max(q, r, 0.9);
+    output = s(z2);
   }
 
-  public void back(double r){
+  public void back(double[][] error){
 
-    double[][] qTarget;
-
-    //max(q, r, gamma)
-
-
-    double[][] sigma4;
     double[][] sigma3;
     double[][] sigma2;
 
-    sigma4 = hMul(subtract(qTarget, oldQ), sPrime(z3));
-    deltaW3 = mul( transpose(a2), sigma4 );
-    sigma3 = hMul( mul(sigma4, transpose(w3)), sPrime(z2));
-    deltaW2 = mul(transpose(a1), sigma3);
-    sigma2 = hMul( mul(sigma3, transpose(w2)), sPrime(z1) );
-    deltaW1 = mul(transpose(s), sigma2);
-
-    //Update oldQ;
-    oldQ = q;
+    sigma3 = hMul(error, sPrime(z2));
+    deltaW2 = mul( transpose(a1), sigma3 );
+    sigma2 = hMul( mul(sigma3, transpose(w2)), sPrime(z1));
+    deltaW1 = mul(transpose(input), sigma2);
 
     //Update W
-    w1 = subtract(w1, sMul(0.5, deltaW1));
-    w2 = subtract(w2, sMul(0.5, deltaW2));
-    w3 = subtract(w3, sMul(0.5, deltaW3));
+    w1 = subtract(w1,  deltaW1);
+    w2 = subtract(w2, deltaW2);
+
   }
 }
--- main.java
+++ main.java
@@ -1,29 +1,34 @@
 public class main{
   public static void main(String[] args){
     NeuNet nn = new NeuNet();
+    double[][] input = new double[1][2];
+    double sum = 0;
+    double[][] error = new double[1][1];
 
+    double count = 0;
 
-    double[][] s = new double[1][5];
-    s[0][0]=1;
-    s[0][1]=2;
-    s[0][2]=3;
-    s[0][2]=3;
-    s[0][2]=3;
+    for(int i = 0; i < 1000000; i++){
 
-    nn.forward(s);
+      input[0][0] = Math.random();
+      input[0][1] = Math.random();
 
+      nn.forward(input);
 
+      sum = input[0][0] + input[0][1];
+      if(sum > 1) sum = 1;
+      else sum = 0;
 
-    nn.back(0.5);
+      error[0][0] = sum - nn.output[0][0];
 
-    int a = 10;
-    int b;
+      if(error[0][0] < Math.pow(10,-5)) count++;
 
-    b = a;
-
-    b++;
-
-    System.out.println(b + "--" + a);
+      nn.back(error);
+      System.out.println("---------------");
+      System.out.println(input[0][0] + "  " + input[0][1]);
+      System.out.println(nn.output[0][0] + "  " +  sum);
+      System.out.println(error[0][0]);
+      System.out.println("---------------");
+    }
 
   }
 }
